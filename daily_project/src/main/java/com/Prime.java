2e9a56a945538b02e922a46080bd8649bd60f359
shivani-kpipartners <shivani.shukla@kpipartners.com>
2022-10-11 15:24:40 +0530

    Commiting Prime

---
+++ daily_project/src/main/java/com/Prime.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Prime {	    
+	     boolean  isPrime(int n) {  
+	               if (n <= 1) {  
+	                   return false;  
+	               }  
+	              // if (n==2) {
+	                  // return true;
+	               //}
+	               for (int i = 2; i < n/2; i++) {  
+	                   if (n % i == 0) {  
+	                       return false;  
+	                   }  
+	               }  
+	               return true;  
+	           }  
+
+	}
